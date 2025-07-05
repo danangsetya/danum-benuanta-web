eaf2001e9341684dce96707b1951a7a6c26181eb
danangsetya <56151256+danangsetya@users.noreply.github.com>
2025-07-06 07:36:07 +0800

    masih kerjain di absensi personal

--- src/app/(main)/absensi/personal/face/error/[...req]/page.tsx
+++ src/app/(main)/absensi/personal/face/error/[...req]/page.tsx
@@ -1,3 +1,8 @@
 export default async function Page({ params }: { params: { req: string[] } }) {
   const timestamp = atob(params.req[0].toString());
+  return (
+    <div>
+      
+    </div>
+  )
 }
